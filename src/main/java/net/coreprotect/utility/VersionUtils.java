3cafd59b140041e1ecf0841a7e15b29170f322a8
albert4719 <mcdoeswhat@qq.com>
2026-06-07 12:17:36 +0800

    Update VersionUtils.java

--- src/main/java/net/coreprotect/utility/VersionUtils.java
+++ src/main/java/net/coreprotect/utility/VersionUtils.java
@@ -104,7 +104,7 @@ public class VersionUtils {
     }
 
     public static boolean isCommunityEdition() {
-        return !isBranch("edge") && !isBranch("coreprotect") && !validDonationKey();
+        return false;
     }
 
     public static boolean isBranch(String branch) {
